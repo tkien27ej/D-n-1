838e12b5e7c0aba26e49a3854801e5ec01e37a46
tkien27ej <tkien4825@gmail.com>
2025-11-12 20:14:58 +0700

    first commit

---
+++ DuAn1_BanAo/src/main/java/com/mycompany/duan1_banao/DuAn1_BanAo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.duan1_banao;
+
+/**
+ *
+ * @author admin
+ */
+public class DuAn1_BanAo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
